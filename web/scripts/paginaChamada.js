0bd45f407e79ee9da0cfe83cfe99122d9380d2b0
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2022-09-12 15:24:01 -0300

    Geração e atualização de QRCode

--- web/scripts/paginaChamada.js
+++ web/scripts/paginaChamada.js
@@ -1,4 +1,5 @@
 //Função de Teste de Requisição para a API
+var showing = false;
 async function getJogos() {
   const response = await fetch("http://54.94.139.104:3000/jogos", {
     method: "GET",
@@ -6,4 +7,53 @@ async function getJogos() {
 
   const data = await response.json();
   console.log(data);
+}
+
+function removeElement(id) {
+  var elem = document.getElementById(id);
+  return elem.parentNode.removeChild(elem);
+}
+var interval;
+const qrcode = document.createElement("div");
+qrcode.className = "qrcode";
+qrcode.id = "qrcode";
+function generateQRCode(){
+  if(!showing){
+    let D = new Date();
+    let t = D.getTime();
+    console.log(t);
+   
+    new QRCode(document.getElementById("qrcode"), "{COD_AULA: 312451,inicio_aula: 314145, fim_aula: 2153413, timestamp_atual:" + t + "}");
+    generatetime = t;
+    showing = true;
+  // Atualiza o qrcode a cada 10 segundos  
+  interval = setInterval(RefreshQRCode,10000);
+
+  }
+  else{
+    //para de atualizar
+    clearInterval(interval);
+    var filhos = document.getElementById("qrcode").childNodes;
+    for(let i = 0;i<2;i++){
+      document.getElementById("qrcode").removeChild(filhos[0]);
+    }
+    showing = false;
+    return;
+  }
+}
+
+function RefreshQRCode(){
+  var filhos = document.getElementById("qrcode").childNodes;
+    for(let i = 0;i<2;i++){
+      document.getElementById("qrcode").removeChild(filhos[0]);
+    }
+    showing = false;
+    let D = new Date();
+    let t = D.getTime();
+    console.log(t);
+   
+    new QRCode(document.getElementById("qrcode"), "{COD_AULA: 312451,inicio_aula: 314145, fim_aula: 2153413, timestamp_atual:" + t + "}");
+    generatetime = t;
+    showing = true;
+    
 }
