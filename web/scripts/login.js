15e29cbb63d4abd650f61fd03b18e5faf398dca9
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2022-09-19 16:52:18 -0300

    checagem de login OK, Header e CSS em manutenção
    Co-authored-by: Mancini <Plendoga@users.noreply.github.com>

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -1,4 +1,20 @@
-//Função de Redirect para a página "paginaChamada.html"
-function Redirect_paginaChamada() {
-  window.location.href = "paginaChamada.html";
+async function checkLogin() {
+  console.log("oi");
+  let logindata = document.getElementsByName("login");
+  const response = await fetch("http://54.94.139.104:3000/professores/" + 
+  logindata[1].value + '/' + 
+  logindata[0].value, {
+    method: "GET",
+  });
+  const data = await response.json();
+  console.log(data);
+  if(!response.ok){
+    alert(data.erro);
+    return(false);
+  }
+  else{
+    window.location.href = "paginaChamada.html";
+    return(true);
+  }
+
 }
