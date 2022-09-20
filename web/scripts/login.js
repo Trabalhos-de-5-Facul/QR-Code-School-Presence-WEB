2d50a843cf5a09d98272496b2605904e1e39b066
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2022-09-20 16:53:59 -0300

    Acertos de css e GETS do BD
    Co-authored-by: Mancini <Plendoga@users.noreply.github.com>

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -1,7 +1,7 @@
 async function checkLogin() {
   console.log("oi");
   let logindata = document.getElementsByName("login");
-  const response = await fetch("http://54.94.139.104:3000/professores/" + 
+  var response = await fetch("http://54.94.139.104:3000/professores/" + 
   logindata[1].value + '/' + 
   logindata[0].value, {
     method: "GET",
@@ -13,6 +13,25 @@ async function checkLogin() {
     return(false);
   }
   else{
+    // response = await fetch("http://54.94.139.104:3000/aulas");
+    // let rd = await response.json();
+    // console.log(rd);
+    // const listaulas = [];
+    // console.log(rd[0]);
+    // for (let i=0;i<rd.quantidade;i++){
+    //   console.log(rd.aulas[i].fk_Professores_COD_PROF);
+    //   if(rd.aulas[i].fk_Professores_COD_PROF == data.cod){
+    //     listaulas.push(rd.aulas[i]);
+    //   }
+    // }
+    // console.log(listaulas);
+    // response = await fetch("http://54.94.139.104:3000/matricula");
+    // let dados = await response.json();
+    // console.log(dados);
+    // const listdisciplinas = [];
+    
+    
+
     window.location.href = "paginaChamada.html";
     return(true);
   }
